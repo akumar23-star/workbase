78f7867b3983e7e3178df4288d1e79583db69c89
Amit <reply2kumarait@gmail.com>
2019-08-24 03:35:00 -0700

    adding customerService using springboot

---
+++ src/main/java/com/colt/ColtcustomerApplication.java
@@ -0,0 +1,15 @@
+package com.colt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColtcustomerApplication{
+
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ColtcustomerApplication.class, args);
+	}
+
+}
